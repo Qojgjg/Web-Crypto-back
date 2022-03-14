1a017a05add6714915d79e5b61a3bf04a1c3921f
Gonzalo <gonzalosola@gmail.com>
2022-03-14 10:03:10 -0300

    Indispensable para levantar (aunque no levanta del todo). Falta Controller

---
+++ src/main/java/ar/crypto/WebCryptoApplication.java
@@ -0,0 +1,13 @@
+package ar.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCryptoApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(WebCryptoApplication.class, args);
+    }
+
+}
