8e8e542433b941f797f67401cc323efb7495530b
Gonzalo Sola <gonzalo.fermin@tata.com.uy>
2022-03-15 09:31:10 -0300

    Modificacion, falta la BD

--- src/main/java/ar/web/crypto/WebCryptoApplication.java
+++ src/main/java/ar/web/crypto/WebCryptoApplication.java
@@ -1,5 +1,17 @@
 package ar.web.crypto;
 
-//@SpringApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan({"ar.web.crypto"})
+@PropertySource("classpath:application.properties")
 public class WebCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCryptoApplication.class, args);
+    }
+
 }
---
+++ src/main/java/ar/web/crypto/controller/WebCryptoController.java
@@ -0,0 +1,10 @@
+package ar.web.crypto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web/crypto/api/v1")
+//@Tag(name = "Controller General", description = "API REST con todas las operaciones de las criptomonedas.")
+public class WebCryptoController {
+}
