8ac84ed5b703e7f57068caf4b2c1078d5895ae2f
Gonzalo <gonzalosola@gmail.com>
2022-03-16 02:18:49 -0300

    Cambio en el Controller. Igual le falta todos los endpoints

--- src/main/java/ar/web/crypto/controller/WebCryptoController.java
+++ src/main/java/ar/web/crypto/controller/WebCryptoController.java
@@ -1,10 +1,13 @@
 package ar.web.crypto.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/web/crypto/api/v1")
-//@Tag(name = "Controller General", description = "API REST con todas las operaciones de las criptomonedas.")
+@Tag(name = "Web Crypto Controller General", description = "API REST con todas las operaciones de las criptomonedas.")
 public class WebCryptoController {
+
+
 }
