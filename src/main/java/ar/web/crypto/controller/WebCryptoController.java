623ed042526d677e67c2a438f44ad261368cc2ff
Gonzalo <gonzalosola@gmail.com>
2022-03-23 02:04:51 -0300

    Elimino JavaExample. Se pasa la invocacion al Controller

--- src/main/java/ar/web/crypto/controller/WebCryptoController.java
+++ src/main/java/ar/web/crypto/controller/WebCryptoController.java
@@ -1,13 +1,65 @@
 package ar.web.crypto.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/web/crypto/api/v1")
-@Tag(name = "Web Crypto Controller General", description = "API REST con todas las operaciones de las criptomonedas.")
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@RestController(value = "Controller de Crypto")
+@RequestMapping(path = "/web/crypto/api/v1")
 public class WebCryptoController {
 
+    public static String apiKey="";
+    @GetMapping(name = "Metodo para probar el servicio este levantado", path = "/ping")
+    public String getPing(){
+        Random r = new Random();
+        String[] lista = {"pong","no jodas","seguis rompiendo las bolas?"};
+        int pos = r.nextInt(lista.length);
+        return lista[pos];
+    }
 
+    @PostMapping(name = "Metodo para probar el servicio este levantado", path = "/ping")
+    public String postCrypto() throws URISyntaxException, IOException {
+        String uri = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+        parameters.add(new BasicNameValuePair("start","1"));
+        parameters.add(new BasicNameValuePair("limit","5000"));
+        parameters.add(new BasicNameValuePair("convert","USD"));
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            HttpEntity entity = response.getEntity();
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+        return response_content;
+    }
 }
